d594ed0dd5a1d3d89c2e94c595092057f72f6d57
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2025-07-19 23:23:58 +0900

    Add Horizontal and Verical Match System

--- Assets/01.Script/Board.cs
+++ Assets/01.Script/Board.cs
@@ -40,6 +40,11 @@ public class Board : MonoBehaviour
         fruitController.CreateFruit(tiles);
     }
     
+    private void Update()
+    {
+        TrySwap();
+    }
+    
     private void CreateTiles()
     {
         tiles = new Tile[boardSize.x * boardSize.y];
@@ -61,7 +66,6 @@ public class Board : MonoBehaviour
         {
             return y * boardSize.x + x;
         }
-        
     }
         
     private Tile FindTile()
@@ -70,11 +74,6 @@ public class Board : MonoBehaviour
             .OrderBy(tile => Vector2.Distance(mousePos, tile.transform.position))
             .FirstOrDefault()
             ?.GetComponentInChildren<Tile>();
-    }
-
-    private void Update()
-    {
-        TrySwap();
     }
     
     private bool TrySwap()
@@ -150,20 +149,125 @@ public class Board : MonoBehaviour
         });
     }
     
-    private bool TryMatch()
-    {
-        
-        
-        
-        return false;
-    }
-    
     private void SwapComplete()
     {
+        Debug.Log("Swap Complete");
+        
         isMoving = false;
         currentFruitIndex = -1;
         lastFruitIndex = -1;
     }
+    
+    private bool TryMatch()
+    {
+        if (CheckHorizontal(currentFruitIndex)) return true;
+        if (CheckHorizontal(lastFruitIndex)) return true;
         
+        if (CheckVertical(currentFruitIndex)) return true;
+        if (CheckVertical(lastFruitIndex)) return true;
+        
+        return false;
+    }
+
+    private bool CheckHorizontal(int index)
+    {
+        //left
+        {
+            int currentX = index;
+            int left = currentX - 1;
+            int left2 = currentX - 2;
+            
+            if (IsTripleMatch(currentX , left ,left2))
+            {
+                return true;
+            }
+        }
+        
+        //center
+        {
+            int currentX = index;
+            int left = currentX - 1;
+            int right = currentX + 1;
+            
+            if (IsTripleMatch(currentX , left ,right))
+            {
+                return true;
+            }
+        }
+        
+        //Right
+        {
+            int currentX = index;
+            int right1 = currentX + 1;
+            int right2 = currentX + 2;
+            
+            if (IsTripleMatch(currentX , right1 ,right2))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool CheckVertical(int index)
+    {
+        //up
+        {
+            int currentY = index;
+            int up1 = currentY - GetBoardWidthSize;
+            int up2 = currentY - GetBoardWidthSize  * 2;
+            
+            if (IsTripleMatch(currentY , up1 ,up2))
+            {
+                return true;
+            }
+        }
+        
+        //center
+        {
+            int currentX = index;
+            int up = currentX - GetBoardWidthSize;
+            int down = currentX + GetBoardWidthSize;
+            
+            if (IsTripleMatch(currentX , up ,down))
+            {
+                return true;
+            }
+        }
+        
+        //Right
+        {
+            int currentX = index;
+            int down = currentX + GetBoardWidthSize;
+            int down2 = currentX + GetBoardWidthSize * 2;
+            
+            if (IsTripleMatch(currentX , down ,down2))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private bool IsTripleMatch(int a, int b, int c)
+    {
+        int maxWidth = tiles.Length;
+        if (a < 0 || b < 0 || c < 0 || a >= maxWidth || b >= maxWidth || c >= maxWidth)
+            return false;
+        
+        var typeA = tiles[a].CurrentFruit.fruitData.fruitType;
+        var typeB = tiles[b].CurrentFruit.fruitData.fruitType;
+        var typeC = tiles[c].CurrentFruit.fruitData.fruitType;
+        
+        bool isMatch = typeA == typeB && typeC == typeC;
+
+        if (isMatch)
+        {
+                        
+        }
+        
+        return isMatch;
+    }
+            
     
 }
